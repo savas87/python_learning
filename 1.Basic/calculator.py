0ff2e9f46c316518f15ac7d9afa41c8da327a22a
savas87 <savas-erbas@live.de>
2020-06-16 13:19:22 +0200

    add a simple calculator

---
+++ 1.Basic/calculator.py
@@ -0,0 +1,19 @@
+# this app is an basic calculator
+# includes some comments, operators and input on terminal
+# at this time i have no idea about functions in python 
+
+# print a comand to input number1
+print("Please enter number1")
+number1 = float(input())
+
+# print a comand to input number2
+print("Please enter number2")
+number2 = float(input())
+
+# print result of all mathematics operators on basic
+print("Addition: ", number1 + number2)
+print("Subtraction: ", number1 - number2)
+print("Division: ", number1 / number2)
+print("Division without after comma: ", number1 // number2)
+print("Multiplication: ", number1 * number2)
+print("Modulo: ", number1 % number2)
